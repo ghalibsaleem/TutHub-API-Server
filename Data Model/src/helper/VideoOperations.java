6ad3eadb3ff535a823057816eaff53ee2fbc867f
Ghalib Saleem <ghalib.saleem.123@gmail.com>
2016-03-05 20:56:55 +0530

    blah blah blah

--- Data Model/src/helper/VideoOperations.java
+++ Data Model/src/helper/VideoOperations.java
@@ -13,7 +13,7 @@ import java.util.List;
  * Created by ghalib on 2/10/2016.
  */
 public class VideoOperations {
-    public List<Video> getVideoByCourse(String courseId,ACCESS_TYPE access_type){
+    public List<Video> getVideoByCourse(String courseId){
         if (Helper.sessionFactory == null)
             Helper.init();
         Session session = Helper.sessionFactory.openSession();
@@ -22,11 +22,10 @@ public class VideoOperations {
             query.setParameter("courseId",courseId);
             Course course = (Course) query.uniqueResult();
             List<Video> videoList = course.getVideos();
-            if (access_type == ACCESS_TYPE.NONE || access_type == ACCESS_TYPE.TRAIL)
-                for (Video video:videoList) {
-                    video.setExcerciseUrl("");
-                    video.setVideoUrl("");
-                }
+            for (Video video:videoList) {
+                video.setExcerciseUrl("");
+                video.setVideoUrl("");
+            }
             return course.getVideos();
         }catch (Exception e){
             e.printStackTrace();
